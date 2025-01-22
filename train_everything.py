d53a6c668eaf6515fc3e0538212b37291a48ae1a
Rémi de Vergnette <remi.vergnette@gmail.com>
2025-01-22 23:31:47 +0100

    Bootstrap

--- train_everything.py
+++ train_everything.py
@@ -160,172 +160,177 @@ domain_codes_asp = [
     "bluetooth", "boots", "bags_and_cases", "vacuums", "tv", "keyboards"
 ]
 domain_codes_json = ["bt", "boots", "bag", "vacuum", "tv", "keyboard"]
-for domain_code_asp, domain_code_json in zip(domain_codes_asp,
-                                             domain_codes_json):
-    model = RobertaForSequenceClassification.from_pretrained(
-        model_name,
-        problem_type="multi_label_classification",
-        num_labels=num_labels)
+for counter in range(20):
+    for domain_code_asp, domain_code_json in zip(domain_codes_asp,
+                                                domain_codes_json):
+        model = RobertaForSequenceClassification.from_pretrained(
+            model_name,
+            problem_type="multi_label_classification",
+            num_labels=num_labels)
 
-    model.to(device)
+        model.to(device)
 
-    tokenizer = RobertaTokenizerFast.from_pretrained(model_name)
+        tokenizer = RobertaTokenizerFast.from_pretrained(model_name)
 
-    #model.load_state_dict (torch.load ("models/pytorch_model_bags.bin", map_location=device) )
+        #model.load_state_dict (torch.load ("models/pytorch_model_bags.bin", map_location=device) )
 
-    ### TO CHANGE ###
-    dev_dataset = TextLabelDataset(file_path=f"data/{domain_code_asp}-dev.asp",
-                                   keep_all=True)
-    test_dataset = TextLabelDataset(
-        file_path=f"data/{domain_code_asp}-tst.asp", keep_all=True)
+        ### TO CHANGE ###
+        dev_dataset = TextLabelDataset(file_path=f"data/{domain_code_asp}-dev.asp",
+                                    keep_all=True)
+        test_dataset = TextLabelDataset(
+            file_path=f"data/{domain_code_asp}-tst.asp", keep_all=True)
 
-    train_size = int(0.75 * len(dev_dataset))
-    val_size = len(dev_dataset) - train_size
-    train_dataset, val_dataset = random_split(dev_dataset,
-                                              [train_size, val_size])
-    ##################
+        train_size = int(0.75 * len(dev_dataset))
+        val_size = len(dev_dataset) - train_size
+        train_dataset, val_dataset = random_split(dev_dataset,
+                                                [train_size, val_size])
+        ##################
 
-    general_aspect_label = dev_dataset.general_label
+        general_aspect_label = dev_dataset.general_label
 
-    train_dataloader = DataLoader(train_dataset,
-                                  batch_size=4,
-                                  shuffle=True,
-                                  collate_fn=collate_fn)
-    test_dataloader = DataLoader(test_dataset,
-                                 batch_size=4,
-                                 shuffle=False,
-                                 collate_fn=collate_fn)
-    val_dataloader = DataLoader(val_dataset,
-                                batch_size=4,
-                                shuffle=False,
-                                collate_fn=collate_fn)
+        train_dataloader = DataLoader(train_dataset,
+                                    batch_size=4,
+                                    shuffle=True,
+                                    collate_fn=collate_fn)
+        test_dataloader = DataLoader(test_dataset,
+                                    batch_size=4,
+                                    shuffle=False,
+                                    collate_fn=collate_fn)
+        val_dataloader = DataLoader(val_dataset,
+                                    batch_size=4,
+                                    shuffle=False,
+                                    collate_fn=collate_fn)
 
-    GRADIENT_ACCUMULATION_STEPS = 1
+        GRADIENT_ACCUMULATION_STEPS = 1
 
-    NUM_TRAIN_EPOCHS = 20
-    LEARNING_RATE = 1e-5
-    MAX_GRAD_NORM = 15
-    BATCH_SIZE = 32
+        NUM_TRAIN_EPOCHS = 20
+        LEARNING_RATE = 1e-5
+        MAX_GRAD_NORM = 15
+        BATCH_SIZE = 32
 
-    num_train_steps = int(
-        len(train_dataloader.dataset) / BATCH_SIZE /
-        GRADIENT_ACCUMULATION_STEPS * NUM_TRAIN_EPOCHS)
+        num_train_steps = int(
+            len(train_dataloader.dataset) / BATCH_SIZE /
+            GRADIENT_ACCUMULATION_STEPS * NUM_TRAIN_EPOCHS)
 
-    param_optimizer = list(model.named_parameters())
-    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-    optimizer_grouped_parameters = [{
-        'params':
-        [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-        'weight_decay':
-        0.01
-    }, {
-        'params':
-        [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
-        'weight_decay':
-        0.0
-    }]
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [{
+            'params':
+            [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+            'weight_decay':
+            0.01
+        }, {
+            'params':
+            [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+            'weight_decay':
+            0.0
+        }]
 
-    optimizer = AdamW(optimizer_grouped_parameters, lr=LEARNING_RATE)
-    #scheduler = WarmupLinearSchedule(optimizer, warmup_steps=num_warmup_steps, t_total=num_train_steps)
+        optimizer = AdamW(optimizer_grouped_parameters, lr=LEARNING_RATE)
+        #scheduler = WarmupLinearSchedule(optimizer, warmup_steps=num_warmup_steps, t_total=num_train_steps)
 
-    OUTPUT_DIR = "models/"
-    MODEL_FILE_NAME = "pytorch_model.bin"
-    PATIENCE = 16
+        OUTPUT_DIR = "models/"
+        MODEL_FILE_NAME = "pytorch_model.bin"
+        PATIENCE = 16
 
-    loss_history = []
-    no_improvement = 0
-    for _ in trange(int(NUM_TRAIN_EPOCHS), desc="Epoch"):
-        model.train()
-        tr_loss = 0
-        nb_tr_examples, nb_tr_steps = 0, 0
-        for step, batch in enumerate(
-                tqdm(train_dataloader, desc="Training iteration")):
-            batch = tuple(t.to(device) for t in batch)
-            input_ids, input_mask, label_ids = batch
+        loss_history = []
+        no_improvement = 0
+        for _ in trange(int(NUM_TRAIN_EPOCHS), desc="Epoch"):
+            model.train()
+            tr_loss = 0
+            nb_tr_examples, nb_tr_steps = 0, 0
+            for step, batch in enumerate(
+                    tqdm(train_dataloader, desc="Training iteration")):
+                batch = tuple(t.to(device) for t in batch)
+                input_ids, input_mask, label_ids = batch
 
-            outputs = model(input_ids,
-                            attention_mask=input_mask,
-                            labels=label_ids)
-            loss = outputs.loss
+                outputs = model(input_ids,
+                                attention_mask=input_mask,
+                                labels=label_ids)
+                loss = outputs.loss
 
-            if GRADIENT_ACCUMULATION_STEPS > 1:
-                loss = loss / GRADIENT_ACCUMULATION_STEPS
+                if GRADIENT_ACCUMULATION_STEPS > 1:
+                    loss = loss / GRADIENT_ACCUMULATION_STEPS
 
-            loss.backward()
-            tr_loss += loss.item()
+                loss.backward()
+                tr_loss += loss.item()
 
-            if (step + 1) % GRADIENT_ACCUMULATION_STEPS == 0:
+                if (step + 1) % GRADIENT_ACCUMULATION_STEPS == 0:
 
-                optimizer.step()
-                optimizer.zero_grad()
-                #scheduler.step()
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    #scheduler.step()
 
-        dev_loss, f1 = evaluate_accuracy(model, val_dataloader)
-        """print("Loss history:", loss_history)
-            print("Dev loss:", dev_loss)
-            print("Dev f1:", f1)
-            """
-        if len(loss_history) == 0 or f1 > max(loss_history):
-            no_improvement = 0
-            model_to_save = model.module if hasattr(model, 'module') else model
-            output_model_file = os.path.join(OUTPUT_DIR, MODEL_FILE_NAME)
-            torch.save(model_to_save.state_dict(), output_model_file)
-        else:
-            no_improvement += 1
+            dev_loss, f1 = evaluate_accuracy(model, val_dataloader)
+            """print("Loss history:", loss_history)
+                print("Dev loss:", dev_loss)
+                print("Dev f1:", f1)
+                """
+            if len(loss_history) == 0 or f1 > max(loss_history):
+                no_improvement = 0
+                model_to_save = model.module if hasattr(model, 'module') else model
+                output_model_file = os.path.join(OUTPUT_DIR, MODEL_FILE_NAME)
+                torch.save(model_to_save.state_dict(), output_model_file)
+            else:
+                no_improvement += 1
 
-        if no_improvement >= PATIENCE:
-            #print("No improvement on development set. Finish training.")
-            break
+            if no_improvement >= PATIENCE:
+                #print("No improvement on development set. Finish training.")
+                break
 
-        loss_history.append(f1)
+            loss_history.append(f1)
 
-    model.load_state_dict(
-        torch.load("models/pytorch_model.bin", map_location=device))
-    mbce, f1 = evaluate_accuracy(model, test_dataloader)
-    losses_df.loc[domain_code_asp, "f1"] = f1
-    rouge = load("rouge")
-    """
-        CHANGE ME
-        THIS NEXT LINE NEEDS TO BE CHANGED WHEN ON A DIFFERENT DOMAIN
+        model.load_state_dict(
+            torch.load("models/pytorch_model.bin", map_location=device))
+        mbce, f1 = evaluate_accuracy(model, test_dataloader)
+        losses_df.loc[domain_code_asp, "f1"] = f1
+        rouge = load("rouge")
         """
-    for dataset_file in [f'data/oposum/{domain_code_json}/test.json']:
-        oposum = OposumReviews(file_path=dataset_file)
-        all_sorted_sentences = []
-        predictions = []
-        references = []
-        coeffs = [(0, 1), (1, 0), (1, 1)]
-        for pipe_index, coeff in enumerate(coeffs):
-            for sentences, reviews in oposum:
-                pol = get_polarity_batched(sentences).cpu()
-                aspects = classify(sentences)
-                spikedness = (aspects[1].cpu().max(dim=1)[0] -
-                              aspects[1].cpu()[:, general_aspect_label])
-                spikedness = spikedness / spikedness.mean()
-                salience = spikedness**(coeff[0]) * pol.abs()**coeff[1]
-                salience_ = sorted([(i, s) for i, s in enumerate(salience)],
-                                   key=lambda x: x[1],
-                                   reverse=True)
-                sorted_sentences = [sentences[i] for i, s in salience_]
-                all_sorted_sentences.append(sorted_sentences)
-                sim = compute_cosine_similarity(sorted_sentences)
-                prediction = ""
-                indexes = []
-                for i in range(6):
-                    if prediction == "" or max([sim[i][j]
-                                                for j in indexes]) <= 0.45:
-                        indexes.append(i)
-                        prediction += sorted_sentences[i]
+            CHANGE ME
+            THIS NEXT LINE NEEDS TO BE CHANGED WHEN ON A DIFFERENT DOMAIN
+            """
+        for dataset_file in [f'data/oposum/{domain_code_json}/test.json']:
+            oposum = OposumReviews(file_path=dataset_file)
+            all_sorted_sentences = []
+            predictions = []
+            references = []
+            coeffs = [(0, 1), (1, 0), (1, 1)]
+            for pipe_index, coeff in enumerate(coeffs):
+                for sentences, reviews in oposum:
+                    pol = get_polarity_batched(sentences).cpu()
+                    aspects = classify(sentences)
+                    spikedness = (aspects[1].cpu().max(dim=1)[0] -
+                                aspects[1].cpu()[:, general_aspect_label])
+                    spikedness = spikedness / spikedness.mean()
+                    salience = spikedness**(coeff[0]) * pol.abs()**coeff[1]
+                    salience_ = sorted([(i, s) for i, s in enumerate(salience)],
+                                    key=lambda x: x[1],
+                                    reverse=True)
+                    sorted_sentences = [sentences[i] for i, s in salience_]
+                    all_sorted_sentences.append(sorted_sentences)
+                    sim = compute_cosine_similarity(sorted_sentences)
+                    prediction = ""
+                    indexes = []
+                    for i in range(6):
+                        if prediction == "" or max([sim[i][j]
+                                                    for j in indexes]) <= 0.45:
+                            indexes.append(i)
+                            prediction += sorted_sentences[i]
 
-                    if len(indexes) >= 5:
-                        break
-                predictions.append(prediction)
-                references.append(reviews)
-            rouge_metrics = rouge.compute(references=references,
-                                          predictions=predictions,
-                                          use_stemmer=True)
-            for metric in metric_names:
-                metrics[metric].loc[domain_code_asp,
-                                    pipes[pipe_index]] = rouge_metrics[metric]
-for key, value in metrics.items():
-    value.to_csv(f"{key}.csv")
-losses_df.to_csv("loss.csv")
+                        if len(indexes) >= 5:
+                            break
+                    predictions.append(prediction)
+                    references.append(reviews)
+                rouge_metrics = rouge.compute(references=references,
+                                            predictions=predictions,
+                                            use_stemmer=True)
+                for metric in metric_names:
+                    if counter == 0:
+                        metrics[metric].loc[domain_code_asp,
+                                        pipes[pipe_index]] = rouge_metrics[metric]
+                    else:
+                        metrics[metric].loc[domain_code_asp,
+                                        pipes[pipe_index]] += rouge_metrics[metric]
+    for key, value in metrics.items():
+        value.to_csv(f"{key}.csv")
+    losses_df.to_csv("loss.csv")
