b258db13928b92825b395d6c96d9abe65a9c63cd
Rémi de Vergnette <remi.vergnette@gmail.com>
2025-01-23 15:13:19 +0100

    automatic coeff

--- aggregate.py
+++ aggregate.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import numpy as np
+from math import ceil
 
 metrics = ["rouge1", "rouge2", "rougeL"]
 
@@ -10,13 +11,14 @@ metric_baselines = {}
 for metric in metrics:
     metric_models[metric] = pd.read_csv(f"{metric}.csv",index_col=0)
     metric_baselines[metric] = pd.read_csv(f"baselines_{metric}.csv",index_col=0)
-
+coeff = ceil(2 * metric_models["rouge1"].loc["bluetooth","P"])
 for domain in metric_models ["rouge1"].index:
     print(domain.replace("_","\\_"), end=" & ")
     gout = []
     for metric in metrics:
         m = (metric_models[metric].loc[domain].values.tolist())
         b = (metric_baselines[metric].loc[domain].values.tolist())
+        m = [m_/coeff for m_ in m]
         v = m+b
         mm = np.argmax(v)
         outl = [f"{val:0.2f}" for val in v]
